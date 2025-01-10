018d5461cf51c8affcfafd48290efdddf0748b9a
Tomas Toledo <trtoledo1977@gmail.com>
2025-01-10 13:57:45 -0300

    styles

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const heading = document.querySelector("h1");
-console.log(heading);
-heading.style.color = "green";
-heading.style.fontSize = "70px";
+const headingTwo = document.querySelector("h2");
+const paragraphs = document.querySelectorAll("p");
+const demoParagraphs = document.querySelectorAll("p.demo-class");
+console.log(demoParagraphs);
+console.log(paragraphs);
+// paragraphs.forEach((para) => para.classList.add("body-text"));
+demoParagraphs.forEach((para) => para.classList.add("body-text"));
+
+// console.log(heading);
+// heading.classList.add("blue");
+// heading.style.color = "green";
+// heading.style.fontSize = "70px";
+headingTwo.innerText = "World";
+heading.classList.add("blue");
